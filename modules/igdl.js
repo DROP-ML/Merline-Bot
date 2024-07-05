38b761f0cd732d6342ff7ba4a90ebeb5cd17048b
Code DROPML <88972255+SAMPATH-0522@users.noreply.github.com>
2024-07-05 18:00:35 +0530

    .

--- modules/igdl.js
+++ modules/igdl.js
@@ -5,6 +5,7 @@ const { react, sendM, sendVideomp4 } = require('../handler/sendFunction');
 const fs = require('fs').promises; // Use fs.promises for async file operations
 const randomNumber = Math.floor(Math.random() * 10) + 1;
 const videoFileName = `${randomNumber}.mp4`;
+const { ndown } = require("nayan-media-downloader")
 
 async function igdl(sock, m, M, text) {
 
@@ -18,13 +19,13 @@ async function igdl(sock, m, M, text) {
   }
   try {
 
-    const response = await axios.get(`https://aemt.me/download/igdl?url=${url}`);
-    const data = response.data;
+    let URL = await ndown("https://www.instagram.com/reel/C5f8uIYhP94/?igsh=cGw3eDVjZGJyMnA3");
+
 
     if (data.code == 200) {
       react(sock, m,M, lang.react.down);
 
-      const videoResponse = await axios.get(data.result[0].url, { responseType: 'arraybuffer' });
+      const videoResponse = await axios.get(URL.data[0].url, { responseType: 'arraybuffer' });
 
       await fs.writeFile(videoFileName, Buffer.from(videoResponse.data));
       react(sock, m,M, lang.react.process);
@@ -35,12 +36,12 @@ async function igdl(sock, m, M, text) {
       await fs.unlink(videoFileName);
     } else {
       react(sock, m, M, lang.react.error)
-      await sendM(sock, m, M, "*IG Reel Is Can't find ...*")
+      await sendM(sock, m, M, "*IG Reel Can't find ...*")
     }
   } catch (error) {
     console.error('Error during Instagram video download:', error.message || error);
     react(sock, m, M, lang.react.error)
-    sendM(sock, m, M, "*I Can't do it right now*")
+    sendM(sock, m, M, "*I Can't do This right now*")
   }
 }
 
