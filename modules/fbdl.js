03da1bbbc9cdd3f152bc6f482237556ad2a5b3bf
Code DROPML <88972255+SAMPATH-0522@users.noreply.github.com>
2024-07-09 14:13:36 +0530

    Updated fb

--- modules/fbdl.js
+++ modules/fbdl.js
@@ -1,15 +1,15 @@
 const axios = require('axios');
 const lang = require('../handler/lang.json');
-const {cv} = require('./catch');
+const { cv } = require('./catch');
 const { react, sendM, sendVideomp4 } = require('../handler/sendFunction');
 const fbdl_v2 = require('./fbdl-v2');
-const {ndown} = require("nayan-media-downloader")
+const { ndown } = require("nayan-media-downloader");
 const fs = require('fs').promises; // Use fs.promises for async file operations
 const randomNumber = Math.floor(Math.random() * 100000) + 1;
 const videoFileName = `${randomNumber}.mp4`;
 
 async function fbdl(sock, m, M, text) {
-  react(sock, m,M, lang.react.process);
+  react(sock, m, M, lang.react.process);
 
   var url2 = text
   let hasPipe = url2.includes("|");
@@ -32,17 +32,17 @@ async function fbdl(sock, m, M, text) {
   };
   try {
     // const response = await axios.request(options);
-    
-let res = ndown(url2, {version: "v1" }); //  version: "v1" | "v2" | "v3"
+
+    let res = await ndown(url2, { version: "v1" }); //  version: "v1" | "v2" | "v3"
     const videoResponse = await axios.get(res.data[0].url, { responseType: 'arraybuffer' });
     await fs.writeFile(videoFileName, Buffer.from(videoResponse.data));
-    react(sock, m,M, lang.react.upload);
-    await sendVideomp4(sock,m,M,videoFileName,tex)
+    react(sock, m, M, lang.react.upload);
+    await sendVideomp4(sock, m, M, videoFileName, tex)
 
-    react(sock, m,M, lang.react.success);
+    react(sock, m, M, lang.react.success);
     await fs.unlink(videoFileName);
   } catch (error) {
-    fbdl_v2(sock,m,M,text)
+    fbdl_v2(sock, m, M, text)
   }
 }
 
