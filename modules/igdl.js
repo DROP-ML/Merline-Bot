ac9129d4e7bb8c93c6364602fe87b9185442a5a4
Code DROPML <88972255+SAMPATH-0522@users.noreply.github.com>
2024-12-05 17:45:32 +0530

    ig

--- modules/igdl.js
+++ modules/igdl.js
@@ -81,22 +81,35 @@ async function igdl(sock, m, M, text) {
     })
   };
 
-  try {
-    const response = await fetch(url5, options);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+  const url6 = 'https://social-all-in-one.p.rapidapi.com/info?url='+url+'&format=json';
+  const options6 = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': '92099ecd2cmsh20ff35cd2120ab7p18335bjsn60b078305587',
+      'x-rapidapi-host': 'social-all-in-one.p.rapidapi.com',
+      'Content-Type': 'application/json'
     }
-    const result = await response.json(); // Parse response as JSON
-    
+  };
 
+  try {
+    let response1;
+    let response2;
+    response1 = await fetch(url5, options);
+    if (response1.status == 429) {
+      response2 = await fetch(url6, options6);
+    }
+    const result1 = await response1.json(); // Parse response as JSON
+    const result2 = await response2.json(); // Parse response as JSON
+
+    let gg = response1.status == 429 ? result1.formats[0].url : result2.medias[0].url;
 
     // Fetch and save the video
-    const videoResponse = await axios.get(result.medias[0].url, { responseType: 'arraybuffer' });
+    const videoResponse = await axios.get(gg, { responseType: 'arraybuffer' });
     await fs.writeFile(videoFileName, Buffer.from(videoResponse.data));
 
     // Send the video to the chat
     react(sock, m, M, emoji());
-    await sendVideomp4(sock, m, M, videoFileName,tex);
+    await sendVideomp4(sock, m, M, videoFileName, tex);
 
     // Clean up the temporary video file
     await fs.unlink(videoFileName);
